9d98762878c405828b2e97ef521de2f9abcec2d8
pjione <a41924250@gmail.com>
2023-04-04 11:11:25 +0900

    test

--- CarShop/src/main/java/com/carshop/controller/mapper/CarRepository.java
+++ CarShop/src/main/java/com/carshop/controller/mapper/CarRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.carshop.controller.CarDTO;
 
@@ -12,11 +13,14 @@ import com.carshop.controller.CarDTO;
 public interface CarRepository {
 	@Select("select cprice from car")
 	List<CarDTO> getAllCarList();
+	
+	
 	List<CarDTO> getCarListByCategory(String category);
 	CarDTO getCarById(@Param("cid") String carId);
 	
 	int deleteCar(@Param("cid") String carId);
 	int setNewCar(CarDTO car);
+	@Update("")
 	void setUpdateCar(CarDTO car);
 }
 	
